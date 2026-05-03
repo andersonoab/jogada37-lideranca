6df9b6afee8e9c45793334f99b52dd139a445294
Anderson Marinho - Advogado <oab.adv.anderson@gmail.com>
2026-05-02 21:54:12 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const APP_CONFIG = {
   SITE_URL: "https://andersonoab.github.io/jogada37-lideranca/",
-  APPS_SCRIPT_URL: "",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzyqpFmrV9qItp0ge1NXK08PZpCnXycPt9wPQghutXECFOD4ltZDlwWFbGHh-M5RVKx/exec",
   EVENTO: "A Jogada 37 da Liderança",
   EMPRESA: "Sonova Retail",
   TEMPO_QR_SEGUNDOS: 90,
